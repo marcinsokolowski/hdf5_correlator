e6975fd030a179b80d32d30cbec0d2b0a8235204
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-13 10:16:52 +0800

    list file added

--- scripts/beamform_all.sh
+++ scripts/beamform_all.sh
@@ -15,12 +15,21 @@ if [[ -n "$3" && "$3" != "-" ]]; then
    do_phase_calib=$3
 fi
 
+tmp_list=`mktemp`
+ls channel_cont_*.hdf5 > ${tmp_list}
+if [[ -n "$4" && "$4" != "-" ]]; then
+   echo "External list file provided"
+   echo "cp $4 ${tmp_list}"
+   cp $4 ${tmp_list}
+fi
+
+
 echo "rm -f ${outfile}"
 rm -f ${outfile}
 
 #!/bin/bash
 # was : for hdf5file in `ls -tr channel_cont_*.hdf5`
-for hdf5file in `ls channel_cont_*.hdf5`
+for hdf5file in `cat ${tmp_list}`
 do
    out_txtfile=${hdf5file%%hdf5}txt
    # echo "hdf5_correlator ${hdf5file} -B -o beamformed2.txt -f -x 1000"
