c34b08da54661d94fa58f4bddde7ca86cc6daafb
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-18 10:00:03 +0800

    improvement

--- scripts/get_last_calib.sh
+++ scripts/get_last_calib.sh
@@ -10,14 +10,19 @@ if [[ -n "$2" && "$2" != "-" ]]; then
    pol=$2
 fi
 
-caldir=/data/real_time_calibration/
+dt="2???_??_??"
 if [[ -n "$3" && "$3" != "-" ]]; then
-   caldir=$3
+  dt=$3
+fi
+
+caldir=/data/real_time_calibration/
+if [[ -n "$4" && "$4" != "-" ]]; then
+   caldir=$4
 fi
 
 # lastcal=`ls -d 2???_??_??-??:?? | tail -1` # I am assuming the SKA will not work beyond year 3000 ...
 
-lastcal=`ls ${caldir}/2???_??_??-??:??/chan_${ch}_selfcal_pha_${pol}.txt | tail -1`
+lastcal=`ls ${caldir}/${dt}-??:??/chan_${ch}_selfcal_pha_${pol}.txt | tail -1`
 # lastcal_YY=`ls ${caldir}/2???_??_??-??:??/chan_${ch}_selfcal_pha_YY.txt | tail -1`
 
 awk '{for(i=3;i<=NF;i++){printf("%.2f,",$i);}}' ${lastcal}
