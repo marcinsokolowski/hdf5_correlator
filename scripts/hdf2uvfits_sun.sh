df2bbbf967265dd4e73e5c2301cdce2bb6c3a455
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2019-12-19 10:15:05 +0800

    corrections and version for aavs2 added

--- scripts/hdf2uvfits_sun.sh
+++ scripts/hdf2uvfits_sun.sh
@@ -7,5 +7,6 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    options="$1"
 fi
 
+echo "hdf5_to_uvfits_all.sh -c -l -i 0.2831 -n 8140 -d "./" -N ${options}"
 hdf5_to_uvfits_all.sh -c -l -i 0.2831 -n 8140 -d "./" -N ${options}
 
---
+++ scripts/hdf2uvfits_zenith_aavs2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1 channel : -b 1 
+# -a to process correlated hdf5 files
+options=""
+if [[ -n "$1" && "$1" != "-" ]]; then
+   options="$1"
+fi
+
+echo "hdf5_to_uvfits_all.sh -c -l -i 0.2831 -n 8140 -d "./" -N -z -s aavs2 ${options}"
+hdf5_to_uvfits_all.sh -c -l -i 0.2831 -n 8140 -d "./" -N -z -s aavs2 ${options}
+
