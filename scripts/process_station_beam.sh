b1eca22cbc216def81734b664ccc33535c899887
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-06 15:09:28 +0800

    max_y=5000

--- scripts/process_station_beam.sh
+++ scripts/process_station_beam.sh
@@ -65,8 +65,8 @@ else
    echo "WARNING : ROOT cern software not installed, skipping plot in ROOT (no worries !)"
 fi
 
-echo "python $beam_scripts_path/plot_power_vs_time.py ${tag}_power_vs_time_ch${freq_channel} --y_min=0 --y_max=1000"
-python $beam_scripts_path/plot_power_vs_time.py ${tag}_power_vs_time_ch${freq_channel} --y_min=0 --y_max=1000
+echo "python $beam_scripts_path/plot_power_vs_time.py ${tag}_power_vs_time_ch${freq_channel} --y_min=0 --y_max=5000"
+python $beam_scripts_path/plot_power_vs_time.py ${tag}_power_vs_time_ch${freq_channel} --y_min=0 --y_max=5000
 
 
 png_file=${tag}_power_vs_time_ch${freq_channel}.png
