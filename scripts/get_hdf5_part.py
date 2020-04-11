cefd4cc55911aa7b7acee5483f9a2773d569b1d7
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-04-11 21:44:59 +0800

    corr

--- scripts/get_hdf5_part.py
+++ scripts/get_hdf5_part.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
    n_integrations = tile0_file['correlation_matrix']['data'].shape[0]
    n_blocks = new_data['root'].attrs['n_blocks']
    new_data['root'].attrs['n_blocks'] = n_integrations
-   print("Changing number of blocks in the %s file from %d -> %d" % (n_blocks,n_integrations))
+   print("Changing number of blocks in the %s file from %d -> %d" % (outfile,n_blocks,n_integrations))
 
 #   f['correlation_matrix']['data'][378:478,:,:,:,]
    alldata = tile0_file['correlation_matrix']['data'][(n_integrations-options.out_n_int):(n_integrations),:,:,:,]
