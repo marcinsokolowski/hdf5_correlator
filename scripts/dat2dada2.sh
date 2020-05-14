6d1e437361d0b2f57ccf40392b730139eb8c3068
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-05-14 13:31:51 +0800

    correction

--- scripts/dat2dada2.sh
+++ scripts/dat2dada2.sh
@@ -9,14 +9,18 @@ path=`which hdf5_to_dada_converter.py`
 
 for datfile in `ls *.dat`
 do
+   unixtime=`echo $datfile | cut -b 11-25`
+   echo "$datfile -> $unixtime - ok ?"
+   sleep 2
+
    outfile=${datfile%%.dat}_${object}.dada   
    hdrfile=${datfile%%.dat}_${object}.hdr
   
 #   echo "python $path ${datfile} --dat2dada --outfile=${outfile}"
 #   python $path ${datfile} --dat2dada --outfile=${outfile}
 
-   echo "python $path ${datfile} --psrdadahdrs --outfile=${hdrfile}"
-   python $path ${datfile} --psrdadahdrs --outfile=${hdrfile}
+   echo "python $path ${datfile} --psrdadahdrs --outfile=${hdrfile} --unixtime=${unixtime}"
+   python $path ${datfile} --psrdadahdrs --outfile=${hdrfile} --unixtime=${unixtime}
    
    
    echo "cat ${hdrfile} ${datfile} > ${outfile}"
