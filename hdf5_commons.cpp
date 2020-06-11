cb096f0fe8be833f9ce13f864d56f661c5568e1f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-06-11 18:03:41 +0800

    corrections

--- hdf5_commons.cpp
+++ hdf5_commons.cpp
@@ -39,6 +39,22 @@ time_t get_unixtime_from_local_string_localfunc( const char* szDTM )
 }
 
 
+time_t get_gmtime_from_string( const char* szGmTime )
+{
+   struct tm gmtime_tm;
+   // sscanf( szGmTime, "%.4u%.2u%.2u_%.2u%.2u%.2u", &gmtime_tm.tm_year,&gmtime_tm.tm_mon,
+   // &gmtime_tm.tm_mday,&gmtime_tm.tm_hour,&gmtime_tm.tm_min,&gmtime_tm.tm_sec);
+
+   strptime( szGmTime, "%Y%m%d_%H%M%S", &gmtime_tm );
+
+   // gmtime_tm.tm_year -= 1900;
+   // gmtime_tm.tm_mon--;
+//   time_t ret = timegm_local( &gmtime_tm );
+   time_t ret = timegm( &gmtime_tm );
+   return ret;
+}
+
+
 
 double hdf5filename2uxtime( const char* filename, const char* format /*="channel_cont_0_%d_%d_0.hdf5"*/, const char* format2 )
 {
@@ -57,9 +73,17 @@ double hdf5filename2uxtime( const char* filename, const char* format /*="channel
    
    char szMidnight[128];
    sprintf(szMidnight,"%d_000000", dtm );
-   time_t uxtime = get_unixtime_from_local_string_localfunc( szMidnight );
+   time_t uxtime = 0;
    
-   printf("TEST uxtime = %d, local_seconds = %d\n",(int)uxtime,local_seconds);
+   if( uxtime >= 1586102400 ){ // from 2020-04-06 Alessio changed file names to have seconds from UTC midnight !!! 
+       // Midnight UTC = 8 am AWST :
+       // this is a temporary fix as it should rather be a new function ....
+       uxtime = get_gmtime_from_string( szMidnight );
+       printf("Using (get_gmtime_from_string) uxtime = %d, ut_seconds = %d\n",(int)uxtime,local_seconds);
+   }else{
+       uxtime = get_unixtime_from_local_string_localfunc( szMidnight );
+       printf("Using (get_gmtime_from_string) uxtime = %d, local_seconds = %d\n",(int)uxtime,local_seconds);
+   }
    
    return (uxtime + local_seconds);
 }
