6794b0885fa72d1b815ea361ecc84a87bd72f317
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-06-12 20:23:41 +0800

    corr

--- scripts/hdf5_to_uvfits_all.sh
+++ scripts/hdf5_to_uvfits_all.sh
@@ -33,7 +33,7 @@ station_name="eda2"
 keep_lfiles=0
 
 if [[ -s /opt/aavs/config/station.yml ]]; then
-   station_name=`awk -v station_section=0 '{if(index($1,":")>0 && NF==1){if(index($1,"station")>0 ){station_section=1;}else{station_section=0;}}if(station_section>0){if($1=="name:"){station_name=$2;gsub("\"","",station_name);print tolower(station_name);}}}' /opt/aavs/config/station.yml`   
+   station_name=`awk -v station_section=0 '{if(index($1,":")>0 && NF==1){if(index($1,"station")>0 ){station_section=1;}else{station_section=0;}}if(station_section>0){if($1=="name:"){station_name=$2;gsub("\"","",station_name);gsub("\r","",station_name);print tolower(station_name);}}}' /opt/aavs/config/station.yml`
    echo "Station config file (or symbolik link) exists -> getting station_name = $station_name"
 else
    echo "WARNING : /opt/aavs/config/station.yml file or symbolic link does not exist will use default station_name = $station_name or value passed in parameter -s"   
--- scripts/station_offline_beamforming.sh
+++ scripts/station_offline_beamforming.sh
@@ -32,7 +32,7 @@ fi
 
 station=eda2
 if [[ -s /opt/aavs/config/station.yml ]]; then
-   station=`awk -v station_section=0 '{if(index($1,":")>0 && NF==1){if(index($1,"station")>0 ){station_section=1;}else{station_section=0;}}if(station_section>0){if($1=="name:"){station_name=$2;gsub("\"","",station_name);print tolower(station_name);}}}' /opt/aavs/config/station.yml`   
+   station=`awk -v station_section=0 '{if(index($1,":")>0 && NF==1){if(index($1,"station")>0 ){station_section=1;}else{station_section=0;}}if(station_section>0){if($1=="name:"){station_name=$2;gsub("\"","",station_name);gsub("\r","",station_name);print tolower(station_name);}}}' /opt/aavs/config/station.yml`
    echo "Station config file (or symbolik link) exists -> getting station_name = $station_name"
 else
    echo "ERROR : /opt/aavs/config/station.yml file or symbolic link does not exist will use default station_name = $station_name or value passed in parameter -s"   
