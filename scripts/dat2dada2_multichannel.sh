fb0bc453f48d6b620b4d885120cda1ca06856717
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2020-09-23 18:37:32 +0800

    corr

--- scripts/dat2dada2_multichannel.sh
+++ scripts/dat2dada2_multichannel.sh
@@ -5,29 +5,24 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    object=$1
 fi
 
-freq_ch=204
-if [[ -n "$2" && "$2" != "-" ]]; then
-   freq_ch=$2
-fi
-
 do_dspsr=1
-if [[ -n "$3" && "$3" != "-" ]]; then
-   do_dspsr=$3
+if [[ -n "$2" && "$2" != "-" ]]; then
+   do_dspsr=$2
 fi
 
 dspsr_options=""
-if [[ -n "$4" && "$4" != "-" ]]; then
-   dspsr_options=$4
+if [[ -n "$3" && "$3" != "-" ]]; then
+   dspsr_options=$3
 fi
 
 conjugate=0
-if [[ -n "$5" && "$5" != "-" ]]; then
-   conjugate=$5
+if [[ -n "$4" && "$4" != "-" ]]; then
+   conjugate=$4
 fi
 
 force=0
-if [[ -n "$6" && "$6" != "-" ]]; then
-   force=$6
+if [[ -n "$5" && "$5" != "-" ]]; then
+   force=$5
 fi
 
 
@@ -48,14 +43,16 @@ echo "###########################################################"
 for ch in `ls -d ???`
 do
    cd ${ch}/
+   ch_val=`echo $ch | awk '{printf("%d",$1);}'`
+   
    for dat_file in `ls *.dat`
    do  
       size_mb=`du -sm $dat_file`
 
       # only process files >500 MB 
       if [[ $size_mb -gt 500 ]];       
-         echo "dat2dada2.sh ${object} ${freq_ch} ${dat_file} ${do_dspsr} "${dspsr_options}" $conjugate $force"
-         dat2dada2.sh ${object} ${freq_ch} ${dat_file} ${do_dspsr} "${dspsr_options}" $conjugate $force
+         echo "dat2dada2.sh ${object} ${ch_val} ${dat_file} ${do_dspsr} "${dspsr_options}" $conjugate $force"
+         dat2dada2.sh ${object} ${ch_val} ${dat_file} ${do_dspsr} "${dspsr_options}" $conjugate $force
       else
          echo "WARNING : file $dat_file smaller than limit -> skipped"
       fi
