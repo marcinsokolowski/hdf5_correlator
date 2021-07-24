b94968f18ac34b3c89f87c580f1eb020b9eed767
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-07-24 15:52:53 +0800

    conjugate parameter added

--- scripts/process_all_objects.sh
+++ scripts/process_all_objects.sh
@@ -5,11 +5,16 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    template="$1"
 fi
 
+conjugate=1
+if [[ -n "$2" && "$2" != "-" ]]; then
+   conjugate=$2
+fi
+
 for object in `ls -d ${template}`
 do
    echo
    echo "----------------------------------------------------------------------------------------------------------------------------"
    date
-   echo "dat2dada2_multifreq.sh $object - 0 > ${object}_processing.out 2>&1"
-   dat2dada2_multifreq.sh $object - 0 > ${object}_processing.out 2>&1
+   echo "dat2dada2_multifreq.sh $object - $conjugate > ${object}_processing.out 2>&1"
+   dat2dada2_multifreq.sh $object - $conjugate > ${object}_processing.out 2>&1
 done
