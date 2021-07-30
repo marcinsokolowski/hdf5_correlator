27fb8cc90865834c74e9170823860fdb58399c6f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2021-07-30 18:35:38 +0800

    +x

--- scripts/process_all_dates.sh
+++ scripts/process_all_dates.sh
@@ -5,3 +5,31 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    datadir=$1
 fi
 
+template="2021*pulsar*"
+if [[ -n "$2" && "$2" != "-" ]]; then
+   template=$2
+fi
+
+conjugate=1
+if [[ -n "$3" && "$3" != "-" ]]; then
+   conjugate=$3
+fi
+
+
+echo "############################################"
+echo "PARAMETERS:"
+echo "############################################"
+echo "datadir   = $datadir"
+echo "template  = $template"
+echo "conjugate = $conjugate"
+echo "############################################"
+date
+
+cd $datadir
+for dir in `ls -d ${template}`
+do
+   cd ${dir}
+   echo "process_all_objects.sh \"J* B*\" ${conjugate}"
+   process_all_objects.sh "J* B*" ${conjugate}   
+   cd ..
+done
