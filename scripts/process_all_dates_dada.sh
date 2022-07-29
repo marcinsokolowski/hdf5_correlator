a092d80413b21fe2ce9c924b5b8b313aa7c5c400
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2022-07-29 13:03:58 +0800

    cosmetics

--- scripts/process_all_dates_dada.sh
+++ scripts/process_all_dates_dada.sh
@@ -100,13 +100,14 @@ do
                   do
                      # channel_1_1_1659060532.876270.dada
                      ch=`echo $dada_file | awk -F '_' '{ch=$2;ux=substr($4,1,17);print ch;}'`
+                     channel_total=`echo "$channel $ch" | awk '{printf("%d\n",($1+$2);}'`
                      freq_mhz=`echo "$channel $ch" | awk '{printf("%.6f\n",($1+$2)*(400.00/512.00));}'`
                      ux=`echo $dada_file | awk -F '_' '{ch=$2;ux=substr($4,1,17);print ux;}'`
                      utc=`date -u -d "1970-01-01 UTC $ux seconds" +"%Y%m%dT%H%M%S"`
-                     outfile=${utc}_ch${ch}.ar
+                     outfile=${utc}_ch${channel_total}.ar
                      
                      echo ".dada file = $dada_file"
-                     echo "ch = $channel + $ch -> freq = $freq_mhz [MHz]"
+                     echo "ch = $channel + $ch = $channel_total -> freq = $freq_mhz [MHz]"
                      echo "ux = $ux -> utc  = $utc"
                      echo "outfile = $outfile"
                   
