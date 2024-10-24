4621363bf00f4ff8c816e48d51706e4b69088a0a
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-10-24 12:22:52 +0800

    added script to merge HDf5 files from multi-channel acquisitions

---
+++ scripts/merge_channel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+channel=0
+if [[ -n "$1" && "$1" != "-" ]]; then
+   channel=$1
+fi
+
+n_channels=2
+if [[ -n "$2" && "$2" != "-" ]]; then
+   n_channels=$2
+fi
+
+
+mkdir -p merged_channel${channel}/
+
+path=`which merge_hdf5_n_chan.py`
+if [[ -n "$path" ]]; then
+   echo "OK : script merge_hdf5_n_chan.py found in $path"
+else
+   echo "ERROR : script merge_hdf5_n_chan.py not found on PATH, trying doing this:"
+   echo "export PATH=~/github/hdf5_correlator/scripts:\$PATH"
+   echo "or"
+   echo "export PATH=~/Software/hdf5_correlator/scripts:\$PATH"
+   echo "or both, or wherever hdf5_correlator package is located"
+fi
+
+for hdf5file in `cat channel_cont_0_*.hdf5`
+do
+   echo "python $path $hdf5file 16 - --outdir=merged_channel${channel}/ --out_channel=${channel} --n_channels=${n_channels}"
+   python $path $hdf5file 16 - --outdir=merged_channel${channel}/ --out_channel=${channel} --n_channels=${n_channels}
+done
